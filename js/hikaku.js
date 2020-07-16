2f2a57201e89f81f37ba9ba5c40f44a1666c362a
kyukyunyorituryo <kyukyunyorituryo@live.jp>
2020-07-16 21:33:05 +0900

    配列を大きい順に並べるようにかえた spliceしたときに順番が変わるため

--- js/hikaku.js
+++ js/hikaku.js
@@ -22,7 +22,17 @@ if(items[j].Asin==zousho[i].ASIN){itemnum=itemnum.concat(j)}
 }
 }
 console.log(itemnum)
+itemnum.sort(function(a,b){
+        if( a > b ) return -1;
+        if( a < b ) return 1;
+        return 0;
+});
 for (var i = 0; i < itemnum.length; i++) {
-items.splice(i,1)
+console.log(items[itemnum[i]])
+items.splice(itemnum[i],1)
 }
+nav=[]
+$('#frame').children().remove();
+templeterender(items)
+for (let i = 0; i < nav.length; i++) {$('#frame').append(nav[i]);}
 }
