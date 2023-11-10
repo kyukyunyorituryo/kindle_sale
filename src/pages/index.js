097ac1d1d373019b142d2de3420d8fe5b27c26a9
急急如律令 <kyukyunyorituryo@live.jp>
2023-11-10 16:10:47 +0900

    セールの追加と削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const BlogIndex = ({ data, location }) => {
 <h1 class="main-heading">Amazon主催のセール</h1>
       <ol style={{ listStyle: `none` }}
       >
-              <List url="https://amzn.to/3MhmhHJ" title="Kindle本高額書籍キャンペーン　最大50％OFF　１１月９日まで" />
+              <List url="https://amzn.to/3u0PAI3" title="Kindle Unlimited定額読み放題" />
                   </ol>
                   <h1 class="main-heading">出版社別のセール</h1>
                   <ol style={{ listStyle: `none` }}
